fca396076caf9eb80911fb8098111c185583b963
suraj kumar <surajbkumar049@gmail.com>
2024-01-16 15:04:13 +0530

    chris second commit

--- src/main/java/com/zettamine/dto/Employee.java
+++ src/main/java/com/zettamine/dto/Employee.java
@@ -1,13 +1,15 @@
 package com.zettamine.dto;
 
 public class Employee {
-<<<<<<< HEAD
+
    public void getEmpByid() {}
    public void getAllEmployees() {}
-=======
+
 	private int empId;
 	private String empName;
 	private float empSal;
-   
->>>>>>> 83869dd25b7cbba165cda08e643b2ea5535d8b5c
+   public void setEmpId(int empId) {
+	   this.empId = empId;
+   }
+
 }
